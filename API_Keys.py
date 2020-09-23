2a2132428cc4abc7399e10aca65bd2588a1884f8
John Chandler <john.chandler@gmail.com>
2020-09-23 08:12:18 -0600

    initial commit

---
+++ API_Keys.py
@@ -0,0 +1,7 @@
+api_key = "1XBBBzcXCGXJUDZwhYmyDXLe2jX"
+api_key_secret = "LAVEAIFH7Hi9jdiDjAidXTzW0ZjUSbQtfpmFO5mMP7aRsyRVKZEPyJ"
+
+access_token = "33022325-wd4EjdhBA5DIkdidPTQfZuJ231uzNQdnglV4uGNvF8N1"
+access_token_secret = "lYVj7lOipZsVsJzdikadiDirfQulGyzUXewAVumLDxIIbsZltv"
+
+
